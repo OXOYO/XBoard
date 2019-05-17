bbaeb4f4d02f5b8edb096dabd4211306603d7359
OXOYO <zmn2007.hi@163.com>
2019-05-17 18:15:38 +0800

    更新。

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/board">Board</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Board.vue
@@ -0,0 +1,17 @@
+/**
+* Created by OXOYO on 2019/5/17.
+*
+* Board
+*/
+
+<style scoped lang="less" rel="stylesheet/less"></style>
+
+<template>
+  <h1>TODO Board</h1>
+</template>
+
+<script>
+  export default {
+    name: 'Board'
+  }
+</script>
