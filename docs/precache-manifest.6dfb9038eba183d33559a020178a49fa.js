8c4f09a1cc6ac229bfa77bae60702a61755f462e
OXOYO <zmn2007.hi@163.com>
2019-05-31 17:29:49 +0800

    打包更新。

---
+++ docs/precache-manifest.6dfb9038eba183d33559a020178a49fa.js
@@ -0,0 +1,64 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0383c2e90130320c7f847cfbc5ba680",
+    "url": "/XBoard/img/iconfont.e0383c2e.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/XBoard/robots.txt"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/XBoard/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "52a03f88787855cfdb5e",
+    "url": "/XBoard/css/app.279f8799.css"
+  },
+  {
+    "revision": "f541f6fad3ac44adfeb6",
+    "url": "/XBoard/css/chunk-vendors.177f70bf.css"
+  },
+  {
+    "revision": "5efa538d3727ec12c740ead4c71aa6f1",
+    "url": "/XBoard/favicon.png"
+  },
+  {
+    "revision": "da9261ef96c046534d3cc51181779a5d",
+    "url": "/XBoard/img/zh-cn.da9261ef.png"
+  },
+  {
+    "revision": "349a281a0c16205ee45bb1fb11c54953",
+    "url": "/XBoard/img/en-us.349a281a.png"
+  },
+  {
+    "revision": "c991cc81abef75162f4865e9c4186fd3",
+    "url": "/XBoard/index.html"
+  },
+  {
+    "revision": "52a03f88787855cfdb5e",
+    "url": "/XBoard/js/app.05a00695.js"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/XBoard/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/XBoard/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/XBoard/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "902e2c0fac696bd3f518",
+    "url": "/XBoard/css/about.619de122.css"
+  },
+  {
+    "url": "/XBoard/chunk-vendors.f541f6fad3ac44adfeb6.js"
+  },
+  {
+    "url": "/XBoard/about.902e2c0fac696bd3f518.js"
+  }
+];
---
+++ docs/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/XBoard/precache-manifest.6dfb9038eba183d33559a020178a49fa.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "XBoard"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
