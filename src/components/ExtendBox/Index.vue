3ca247415a975cec0d7421aa007071f354f5b3f2
OXOYO <zmn2007.hi@163.com>
2019-06-27 15:31:22 +0800

    添加扩展功能。

---
+++ src/components/ExtendBox/Index.vue
@@ -0,0 +1,17 @@
+/**
+* Created by OXOYO on 2019/6/27.
+*
+* ExtendBox
+*/
+
+<style scoped lang="less" rel="stylesheet/less"></style>
+
+<template>
+  <h1>TODO ExtendBox</h1>
+</template>
+
+<script>
+  export default {
+    name: 'ExtendBox'
+  }
+</script>
